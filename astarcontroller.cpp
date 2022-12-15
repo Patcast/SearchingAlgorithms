1e9c9677b075fd656d8af5034b6f5b860f064a7d
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2022-12-15 15:11:08 +0100

    searching algorithm avoids as much as possible specialFigures

--- astarcontroller.cpp
+++ astarcontroller.cpp
@@ -4,22 +4,26 @@
 
 AStarController::AStarController()
 {
-    gameWord_ptr= GameWorld::Instance(":/images/maze3.png",20,30,100.0);
+    gameWord_ptr= GameWorld::Instance(":/images/worldmap.png",10,10,100.0);
     aStar = std::make_unique <AStar>(gameWord_ptr->getTotalRows(),gameWord_ptr->getTotalColumns());
     aStar->setGameWord_ptr(gameWord_ptr);
-    //gameWord_ptr->testing();
+    gameWord_ptr->testing();
+
 }
 
 void AStarController::runDijktra(int start_index, int goal_index)
 {
-
     aStar->printPathFound(aStar->aStarSearch(start_index,goal_index,0.0));
-
 }
 
 void AStarController::runAStar(int start_index, int goal_index,float heuristic)
 {
-//    aStar->printPathFound(aStar->aStarSearch(start_index,goal_index,heuristic));
-    aStar->aStarSearch(start_index,goal_index,heuristic);
+
+    if(start_index<0||start_index>=gameWord_ptr->getTotalColumns()*gameWord_ptr->getTotalRows()||goal_index<0||goal_index>=gameWord_ptr->getTotalColumns()*gameWord_ptr->getTotalRows()){
+       std::cout<<"Searching algorithm: INDEX OUT OF BOUNDS"<<std::endl;
+    }
+//    else  aStar->aStarSearch(start_index,goal_index,heuristic);
+    else aStar->printPathFound(aStar->aStarSearch(start_index,goal_index,heuristic));
+
 
 }
