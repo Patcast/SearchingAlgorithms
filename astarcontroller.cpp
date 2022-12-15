f1a86da845d1c96d69afae2ab5f6fe036964d98d
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2022-12-15 13:20:13 +0100

    minor modification to upgrade search for biggest map

--- astarcontroller.cpp
+++ astarcontroller.cpp
@@ -4,7 +4,7 @@
 
 AStarController::AStarController()
 {
-    gameWord_ptr= GameWorld::Instance(":/images/worldmap.png",20,30,100.0);
+    gameWord_ptr= GameWorld::Instance(":/images/maze3.png",20,30,100.0);
     aStar = std::make_unique <AStar>(gameWord_ptr->getTotalRows(),gameWord_ptr->getTotalColumns());
     aStar->setGameWord_ptr(gameWord_ptr);
     //gameWord_ptr->testing();
@@ -19,5 +19,7 @@ void AStarController::runDijktra(int start_index, int goal_index)
 
 void AStarController::runAStar(int start_index, int goal_index,float heuristic)
 {
-    aStar->printPathFound(aStar->aStarSearch(start_index,goal_index,heuristic));
+//    aStar->printPathFound(aStar->aStarSearch(start_index,goal_index,heuristic));
+    aStar->aStarSearch(start_index,goal_index,heuristic);
+
 }
